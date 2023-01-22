eddd96558195f6bb4bc92504364cb75f9bf248ed
ReVuz <revathymr916@gmail.com>
2023-01-22 19:53:09 +0530

    Java Project Final

---
+++ Blood_bank_management/src/main/java/com/mycompany/blood_bank_management/Blood_bank_management.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blood_bank_management;
+
+/**
+ *
+ * @author Jalaja
+ */
+public class Blood_bank_management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Login_page page1 = new Login_page();
+        page1.show();
+    }
+}
